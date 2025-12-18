2373d28c36fa3a9e6610910cc6226131d04126f9
kozue-y <sep_04_17_jaskoz@icloud.com>
2025-12-18 18:12:11 +0900

    voicesセクションPC

--- js/main.js
+++ js/main.js
@@ -8,3 +8,24 @@ $(".c-drawer-menu__list a").click(function() {
     $("#js-drawer").removeClass("active");
     $(".c-hamburger").removeClass("active");
 });
+
+// スワイパーを初期化
+const swiper = new Swiper(".js-voices-swiper", {
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    // SP1枚、PC3枚
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 35,
+            centeredSlides: false,
+        },
+    },
+
+});
